5813cb84ca377f98e5775480ec47138408f0fae6
Robert Dzienio <robertdzienio@gmail.com>
2021-01-31 17:26:30 +0100

    git init

---
+++ src/main/java/pl/rdzienio/ecommercedemo/EcommerceDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.rdzienio.ecommercedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceDemoApplication.class, args);
+    }
+
+}
